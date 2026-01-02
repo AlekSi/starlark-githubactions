2d42df0ee16202fab39265336fd98ea8f519e38c
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2026-01-02 11:19:40 +0100

    Add example

--- githubactions_test.go
+++ githubactions_test.go
@@ -1,42 +1,59 @@
-package githubactions
+package githubactions_test
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	githubactions "github.com/AlekSi/starlark-githubactions"
+	gogithubactions "github.com/sethvargo/go-githubactions"
 	"go.starlark.net/starlark"
+	"go.starlark.net/syntax"
 )
 
 func Example() {
-	os.Setenv("GITHUB_EVENT_PATH", "testdata/event.json")
-	defer os.Unsetenv("GITHUB_EVENT_PATH")
+	// Create a Starlark module for this example.
+	getenv := func(key string) string {
+		switch key {
+		case "GITHUB_EVENT_PATH":
+			return "testdata/event.json"
+		default:
+			return ""
+		}
+	}
+	module := githubactions.NewModule(
+		"githubactions",
+		githubactions.New(gogithubactions.New(
+			gogithubactions.WithWriter(os.Stdout),
+			gogithubactions.WithGetenv(getenv),
+		)),
+	)
 
+	// Add module to the predeclared global environment.
+	// Most users should use githubactions.Module instead.
+	predeclared := starlark.StringDict{
+		"githubactions": module,
+	}
+
+	script := `
+def merge_method():
+	ctx = githubactions.context()
+	pr = ctx.event.get("pull_request", {})
+	return pr.get("auto_merge", {}).get("merge_method", "")
+
+print(merge_method())
+`
+
+	opts := &syntax.FileOptions{}
 	thread := &starlark.Thread{
-		Name: "main",
 		Print: func(th *starlark.Thread, msg string) {
 			fmt.Print(msg)
 		},
 	}
-
-	script := `
-def check_pr_title():
-	ctx = githubactions.context()
-	pr = githubactions.context().event.get("pull_request", {})
-	pr_title = pr.get("title", "")
-	print("PR title:", pr_title)
-
-check_pr_title()
-`
-
-	// Execute the script with the githubactions module
-	globals := starlark.StringDict{"githubactions": Module}
-	if _, err := starlark.ExecFile(thread, "pr_check.star", script, globals); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		return
+	if _, err := starlark.ExecFileOptions(opts, thread, "", script, predeclared); err != nil {
+		log.Fatal(err)
 	}
 
-	panic("boom")
-
 	// Output:
-	// PR title: Pull request title
+	// squash
 }
